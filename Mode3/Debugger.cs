d105d1494047db4ae761ae819a3c037e229e0de5
m3hdi <m.mehdimoji@gmail.com>
2020-12-16 21:43:03 +0330

    Debug Mode 3 Buttons(Get Log)

---
+++ Mode3/Debugger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SaveAction(string action)
+    {
+        PlayerPrefs.SetString("LastActions", PlayerPrefs.GetString("LastActions") + '\n' + action);
+    }
+
+    public string ShowActions()
+    {
+        string txt = string.Empty;
+        txt = PlayerPrefs.GetString("LastActions");
+        PlayerPrefs.DeleteKey("LastActions");
+        return txt;
+    }
+}
